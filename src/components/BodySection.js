f4a8d0105945a01c17ccf0f643152090130293b5
Rana Mubashir Tariq <mubashirtariq.1366@gmail.com>
2024-10-01 16:15:38 +0500

    add update files

--- src/components/BodySection.js
+++ src/components/BodySection.js
@@ -10,24 +10,18 @@ export default function BodySection() {
           <img src='/Images/Trans.svg'/>
   </div>
 
-
-      <div className='bottom'>
-      <div className='bottom-heading'>
-         <h2>The Ultimate <br/> gameing ecosystem</h2>
+  <div className='middle-heading-d'>
+       <div className='middle-inner-d'>
+        <div className='middle-inner-heading'><h2>The ultimate gaming ecosystem</h2></div>
+        <div className='middle-inner-para'><p>Step in to bluemoon’s revolutionary gaming ecosystem
+        where you can play, earn & explore digital assets</p></div>
+       </div>
+  </div>
+     <div className='middle-btn-d'>
+      <div className='middle-inner-btn-d'>
+        <button className='middle-btn'><h2>Explore more</h2><img src='/Images/Vector.svg'/></button>
       </div>
-          <div className='bottom-para'>
-          <p>Step in to bluemoon’s revolutionary gaming ecosystem <br/>
-          where you can play, earn & explore digital assets</p>
-          </div>
-          
-           <div className='last-btn'>
-          <div className='outer-btn-d'>
-            <div className='inner-btn-d'>
-              <button>Explore more<img src='/Images/Vector.svg'/></button>
-            </div>
-          </div>
-          </div>
-          </div>
+     </div>
     </div>
   )
 }
