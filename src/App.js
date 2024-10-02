afe01ff1fddbf8a1438cab363bb178f8643dc3b9
Rana Mubashir Tariq <mubashirtariq.1366@gmail.com>
2024-10-02 21:54:10 +0500

    add new data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import BlueEarh from './components/BlueEarh';
 import CardComponent from './components/CardComponent';
 import NavbarSection from './components/NavbarSection';
 function App() {
--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './CardComponent.css'
-import BlueEarh from './BlueEarh'
+
 
 export default function CardComponent() {
   return (
@@ -66,9 +66,70 @@ export default function CardComponent() {
                     <div className='card-para-heading'><p>Store, trade, and utilise your unique digital assets across the Bluemoon gaming ecosystem.</p></div>
                 </div>
              </div>
+
+
         </div>
         
        </div>
-         </div>
+
+       {/* Earth Part */}
+          
+       <div className='Earth-part'>
+          <div className='right-part-d'><img src='/Images/Earth.svg'/></div>
+
+          <div className='left-part-d'>
+              <div className='left-heading-d'>
+                   <div className='left-heading-h2-d'><h2>Explore Bluemoon mobile app</h2></div>
+                   <div className='left-para-d'><p>Engage, Participate and earn <span>xBlue</span></p></div>
+              </div>
+
+               {/* Card Start */}
+
+               <div className='four-card-d'> 
+                         <div className='inner-four-card'>
+                               <div className='inner-card-pic-d'><img src="/Images/Earn.svg" alt="" /> </div>
+                                  <div className='inner-card-data-d'>
+                                       <div className='inner-card-heading-d'><h2>Earn & Collect</h2> </div>
+                                        <div className='inner-card-para-d'><p>Win rewards and earn xBLUE points through app activities and engagement.</p> </div>
+                                   </div>
+                           </div>
+
+                           <div className='inner-four-card'>
+                               <div className='inner-card-pic-d'><img src="/Images/DigitalAssets.svg" alt="" /> </div>
+                                  <div className='inner-card-data-d'>
+                                       <div className='inner-card-heading-d'><h2>Digital & Assets</h2> </div>
+                                        <div className='inner-card-para-d'><p>Own, trade, and display unique assets which you can use in the Bluemoon ecosystem.</p> </div>
+                                   </div>
+                           </div>
+
+
+                           <div className='inner-four-card'>
+                               <div className='inner-card-pic-d'><img src="/Images/Xblue.svg" alt="" /> </div>
+                                  <div className='inner-card-data-d'>
+                                       <div className='inner-card-heading-d'><h2>xBLUE points</h2> </div>
+                                        <div className='inner-card-para-d'><p>Earn xBLUE points everywhere, unlock benefits and exclusive items.</p> </div>
+                                   </div>
+                           </div>
+
+
+
+                           <div className='inner-four-card'>
+                               <div className='inner-card-pic-d'><img src="/Images/LeaderBoard.svg" alt="" /> </div>
+                                  <div className='inner-card-data-d'>
+                                       <div className='inner-card-heading-d'><h2>Leaderboard</h2> </div>
+                                        <div className='inner-card-para-d'><p>Compete globally, climb ranks, and earn recognition in Bluemoon.</p> </div>
+                                   </div>
+                           </div>
+              </div>
+
+
+
+
+               {/* Card End */}
+
+          </div>
+  </div>
+            
+   </div>
   )
 }
